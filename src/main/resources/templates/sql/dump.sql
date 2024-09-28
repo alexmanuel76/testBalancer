73691dc6750eedcc10218238e8d8bb53b1c16156
Alexander Mogollon <alexandermogollon3076@yahoo.com>
2024-09-28 14:02:22 -0400

    Update dump.sql

--- src/main/resources/templates/sql/dump.sql
+++ src/main/resources/templates/sql/dump.sql
@@ -30,7 +30,7 @@ CREATE TABLE `balancer` (
   `hora` date DEFAULT NULL,
   `comentario` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=171 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
